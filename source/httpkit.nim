6185d07a51aed2eee86038b7ad8db61573ee96e0
hugosenari <hugosenari@gmail.com>
2022-09-03 19:56:37 -0300

    feat(nimpkgs): update lock

--- source/httpkit.nim
+++
@@ -1,8 +0,0 @@
-#    HttpKit - An efficient HTTP tool suite written in pure nim
-#        (c) Copyright 2017 Wang Tong
-#
-#    See the file "LICENSE", included in this distribution, for
-#    details about the copyright.
-
-import httpkit.parser
-export httpkit.parser
--- source/test/test.nim
+++
@@ -1,7 +0,0 @@
-#    HttpKit - An efficient HTTP parser written in pure nim
-#        (c) Copyright 2017 Wang Tong
-#
-#    See the file "LICENSE", included in this distribution, for
-#    details about the copyright.
-
-import test_http
